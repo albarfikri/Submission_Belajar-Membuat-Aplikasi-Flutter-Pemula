160b92eccbdc2faa03971717e33f4b110033e2e3
albarfikri <fikrialbar4@gmail.com>
2021-08-03 23:35:03 +0700

    finish creating web version

--- lib/detail_movie_screen.dart
+++ lib/detail_movie_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 import 'model/movie_model.dart';
 
+var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
+
 class DetailMovieScreen extends StatelessWidget {
   final Movie data;
 
@@ -13,11 +15,195 @@ class DetailMovieScreen extends StatelessWidget {
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
         if (constraints.maxWidth > 800) {
-          return DetailMobileScreen(data: data);
+          return DetailWebScreen(data: data);
         } else {
           return DetailMobileScreen(data: data);
         }
       },
+    );
+  }
+}
+
+class DetailWebScreen extends StatefulWidget {
+  final Movie data;
+
+  DetailWebScreen({required this.data});
+
+  @override
+  _DetailWebScreen createState() => _DetailWebScreen();
+}
+
+class _DetailWebScreen extends State<DetailWebScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 64,
+        ),
+        child: Center(
+          child: Container(
+            width: 1200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Detail Movie',
+                  style: TextStyle(
+                    fontFamily: 'Staatliches',
+                    fontSize: 32,
+                  ),
+                ),
+                SizedBox(height: 32),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.network(widget.data.backdrop_path),
+                          ),
+                          SizedBox(height: 16),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 32),
+                    Expanded(
+                      child: Card(
+                        child: Container(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  widget.data.original_title,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 30.0,
+                                    fontFamily: 'Staatliches',
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    flex: 0,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(16.0),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: Image.network(widget.data.poster_path,
+                                            width: 100, height: 150),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(top:16.0),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: <Widget>[
+                                          Container(
+                                            child: Row(
+                                              children: <Widget>[
+                                                Text(
+                                                  widget.data.vote_average,
+                                                  style: TextStyle(
+                                                      fontSize: 14.0, color: Colors.grey),
+                                                ),
+                                                Icon(Icons.favorite,
+                                                    size: 14, color: Colors.redAccent),
+                                              ],
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: 15,
+                                            width: 10,
+                                          ),
+                                          Text(
+                                            widget.data.release_date,
+                                            style: TextStyle(
+                                                fontSize: 12.0, fontWeight: FontWeight.bold),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(16.0),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: <Widget>[
+                                          Text(
+                                            'Detail',
+                                            style: TextStyle(
+                                                fontSize: 18.0, fontWeight: FontWeight.bold),
+                                          ),
+                                          SizedBox(
+                                            height: 15,
+                                            width: 10,
+                                          ),
+                                          Text(
+                                            widget.data.overview,
+                                            style: TextStyle(fontSize: 12.0),
+                                          ),
+                                          SizedBox(
+                                            height: 15,
+                                            width: 10,
+                                          ),
+                                          Text(
+                                            'Vote Count',
+                                            style: TextStyle(
+                                                fontSize: 18.0, fontWeight: FontWeight.bold),
+                                          ),
+                                          SizedBox(
+                                            height: 15,
+                                            width: 10,
+                                          ),
+                                          Container(
+                                            child: Row(
+                                              children: <Widget>[
+                                                Text(
+                                                  widget.data.vote_count,
+                                                  style: TextStyle(
+                                                      fontSize: 14.0, color: Colors.grey),
+                                                ),
+                                                Icon(Icons.people, size: 14, color: Colors.red),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -37,7 +223,9 @@ class DetailMobileScreen extends StatelessWidget {
             children: <Widget>[
               Stack(
                 children: <Widget>[
-                  Image.network(data.backdrop_path),
+                  Expanded(
+                    child: Image.network(data.backdrop_path),
+                  ),
                   SafeArea(
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
@@ -51,7 +239,9 @@ class DetailMobileScreen extends StatelessWidget {
                                 Icons.arrow_back,
                                 color: Colors.black,
                               ),
-                              onPressed: () {},
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
                             ),
                           ),
                           CircleAvatar(
