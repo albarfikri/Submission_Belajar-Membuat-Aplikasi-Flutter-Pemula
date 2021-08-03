58da0ee28017a092967b0eef29051d458360517f
albarfikri <fikrialbar4@gmail.com>
2021-08-03 22:15:15 +0700

    adding search button, and detail screen

--- lib/detail_movie_screen.dart
+++ lib/detail_movie_screen.dart
@@ -1,9 +1,219 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'model/movie_model.dart';
 
 class DetailMovieScreen extends StatelessWidget {
+  final Movie data;
+
+  DetailMovieScreen({required this.data});
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 800) {
+          return DetailMobileScreen(data: data);
+        } else {
+          return DetailMobileScreen(data: data);
+        }
+      },
+    );
+  }
+}
+
+class DetailMobileScreen extends StatelessWidget {
+  final Movie data;
+
+  const DetailMobileScreen({required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Image.network(data.backdrop_path),
+                  SafeArea(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.amberAccent,
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.arrow_back,
+                                color: Colors.black,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                          CircleAvatar(
+                            backgroundColor: Colors.amberAccent,
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.store,
+                                color: Colors.black,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(data.poster_path,
+                            width: 100, height: 150),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            data.original_title,
+                            style: TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 15,
+                            width: 10,
+                          ),
+                          Container(
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  data.vote_average,
+                                  style: TextStyle(
+                                      fontSize: 14.0, color: Colors.grey),
+                                ),
+                                Icon(Icons.favorite,
+                                    size: 14, color: Colors.redAccent),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 15,
+                            width: 10,
+                          ),
+                          Text(
+                            data.release_date,
+                            style: TextStyle(
+                                fontSize: 12.0, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            'Detail',
+                            style: TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 15,
+                            width: 10,
+                          ),
+                          Text(
+                            data.overview,
+                            style: TextStyle(fontSize: 12.0),
+                          ),
+                          SizedBox(
+                            height: 15,
+                            width: 10,
+                          ),
+                          Text(
+                            'Vote Count',
+                            style: TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 15,
+                            width: 10,
+                          ),
+                          Container(
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  data.vote_count,
+                                  style: TextStyle(
+                                      fontSize: 14.0, color: Colors.grey),
+                                ),
+                                Icon(Icons.people, size: 14, color: Colors.red),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: FavButton(),
+      ),
+    );
+  }
+}
+
+class FavButton extends StatefulWidget {
+  @override
+  _FavButtonState createState() => _FavButtonState();
+}
+
+class _FavButtonState extends State<FavButton> {
+  bool isFav = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFav ? Icons.bookmark : Icons.bookmark_border,
+        color: Colors.black,
+      ),
+      onPressed: () {
+        setState(() {
+          isFav = !isFav;
+        });
+      },
+    );
   }
 }
--- lib/landing_screen.dart
+++ lib/landing_screen.dart
@@ -8,99 +8,186 @@ class LandingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            color:Colors.white,
-          ), onPressed: () {  },
-        ),
-        title: Text('Movie Catalogue', style: TextStyle(color: Colors.white)),
-        actions:[
-          IconButton(
-            icon:Icon(
-              Icons.search,
-              color: Colors.white,
-            ), onPressed: () {  },
-          )
-        ],
-      ),
+      appBar: buildAppBar(),
       body: MovieCatalogueList(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child:Icon(Icons.movie),
+        onPressed: () {},
+        child: Icon(Icons.movie),
       ),
     );
   }
 }
 
+AppBar buildAppBar() {
+  return AppBar(
+    elevation: 0,
+    leading: IconButton(
+      icon: Icon(
+        Icons.menu,
+        color: Colors.white,
+      ),
+      onPressed: () {},
+    ),
+  );
+}
+
 class MovieCatalogueList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        final Movie data = moviesList[index];
-        return InkWell(
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return DetailMovieScreen();
-            }));
-          },
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0)
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
+    Size size = MediaQuery.of(context).size;
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: size.height * 0.2 - 27,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  bottom: 36 + 18,
+                ),
+                height: size.height * 0.2 - 27,
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(36),
+                    bottomRight: Radius.circular(36),
+                  ),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      'Movie Catalogue',
+                      style: Theme.of(context).textTheme.headline5!.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 16),
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  height: 55,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 40,
+                          color: Colors.black.withOpacity(0.6)),
+                    ],
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Search",
+                            hintStyle: TextStyle(
+                              color: Colors.amberAccent.withOpacity(0.7),
+                            ),
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.search,
+                          color: Colors.amber,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          child: Container(
+            margin: EdgeInsets.only(top: size.height * 0.2 - 27 + 8),
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                final Movie data = moviesList[index];
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return DetailMovieScreen(data: data);
+                    }));
+                  },
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text(
-                          data.original_title,
-                          style: TextStyle(
-                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text(
+                                  data.original_title,
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                  width: 10,
+                                ),
+                                Container(
+                                  child: Row(
+                                    children: <Widget>[
+                                      Text(
+                                        data.vote_average,
+                                        style: TextStyle(
+                                            fontSize: 14.0, color: Colors.grey),
+                                      ),
+                                      Icon(Icons.favorite,
+                                          size: 14, color: Colors.redAccent),
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                  width: 10,
+                                ),
+                                Text(
+                                  data.release_date,
+                                  style: TextStyle(fontSize: 12.0),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                        SizedBox(
-                          height: 15,
-                          width: 10,
-                        ),
-                        Container(
-                            child: Row(children: <Widget>[
-                          Text(data.vote_average,
-                              style: TextStyle(
-                                  fontSize: 14.0, color: Colors.grey)),
-                          Icon(Icons.star, size: 14, color: Colors.grey),
-                        ])),
-                        SizedBox(
-                          height: 15,
-                          width: 10,
-                        ),
-                        Text(
-                          data.release_date,
-                          style: TextStyle(
-                              fontSize: 12.0),
+                        Expanded(
+                          flex: 0,
+                          child: Image.network(data.poster_path,
+                              width: 100, height: 150),
                         ),
                       ],
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 0,
-                  child:
-                  Image.network(data.poster_path, width: 100, height: 150),
-                )
-              ],
+                );
+              },
+              itemCount: moviesList.length,
             ),
           ),
-        );
-      },
-      itemCount: moviesList.length,
+        ),
+      ],
     );
   }
 }
